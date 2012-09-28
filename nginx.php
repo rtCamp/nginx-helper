7185428bd10052f5163547dafd8ce38ee0727417
Rahul Bansal <rahul.bansal@rtcamp.com>
2012-09-29 01:44:24 +0530

    Readme.txt and screenshot enhancements

--- nginx.php
+++ nginx.php
@@ -3,7 +3,7 @@
 Plugin Name: Nginx Helper
 Plugin URI: http://rtcamp.com/
 Description: An nginx helper that serves various functions.
-Version: 1.0
+Version: 1.1
 Author: rtCamp
 Author URI: http://rtcamp.com
 Requires at least: 3.0
