6c4ce29e8e61d6a25ee91bb4da2d1e91c57b1830
yapapaya <saurabh@yapapaya.com>
2012-10-01 17:18:47 +0530

    "Bug fixes and minor UI revisions"

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # main config
 PLUGINSLUG="nginx-helper"
 CURRENTDIR=`pwd`
-MAINFILE="nginx.php" # this should be the name of your main php file in the wordpress plugin
+MAINFILE="nginx-helper.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
