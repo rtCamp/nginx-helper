4c553801fb237d50cc667ca94b8adc0ca2f0d56b
Mitesh Shah <Mitesh.Shah@rtCamp.com>
2012-11-01 12:29:51 +0530

    Fixed Version Finding

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # main config
 PLUGINSLUG="nginx-helper"
 MAINFILE="nginx-helper.php" # this should be the name of your main php file in the wordpress plugin
-SVNUSER="rtcamp" # your svn username
+#SVNUSER="rtcamp" # your svn username
 
 
 ##### YOU CAN STOP EDITING HERE #####
@@ -18,6 +18,14 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="https://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
 
+# Detect svn username based on url
+SVNUSER=$(cat ~/.subversion/auth/svn.simple/* | grep -A4 $(echo $SVNURL | awk -F// '{print $2}' | cut     -d'/' -f1) | tail -n1)
+if [ -z "$SVNUSER" ]
+then
+	SVNUSER="rtcamp"
+fi
+
+
 # Let's begin...
 echo ".........................................."
 echo
@@ -29,7 +37,8 @@ echo
 # Check version in readme.txt is the same as plugin file
 NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}'`
 echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}'`
+#NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}'`
+NEWVERSION2=`grep -i "Version" $GITPATH/$MAINFILE | head -n1 | awk -F':' '{print $2}' | awk -F' ' '{print $1}'`
 echo "$MAINFILE version: $NEWVERSION2"
 
 if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
@@ -37,7 +46,7 @@ if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exitin
 echo "Versions match in readme.txt and PHP file. Let's proceed..."
 
 cd $GITPATH
-bash readme.sh
+bash readme.sh $SVNURL
 git add README.md
 echo -e "Enter a commit message for this new version: \c"
 read COMMITMSG
