7d8fdfbfe3db1347b3f3c23c9f37dca22f1712b6
Faishal <faishal.saiyed@rtcamp.com>
2014-01-02 18:46:47 +0530

    Update readme

--- nginx-helper.php
+++ nginx-helper.php
@@ -3,7 +3,7 @@
   Plugin Name: Nginx Helper
   Plugin URI: http://rtcamp.com/nginx-helper/
   Description: An nginx helper that serves various functions.
-  Version: 1.7.6
+  Version: 1.8
   Author: rtCamp
   Author URI: http://rtcamp.com
   Text Domain: nginx-helper
