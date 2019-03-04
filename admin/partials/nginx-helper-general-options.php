17054e39fa7df56ec58dca49fad40e620666f4c3
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-03-04 16:42:17 +0530

    Attempt log file creation if parent directory is writable
    PHP throws a warning at fopen line that it failed to open log file. This
    happens when program tries to create a file but parent directory is not
    writable.
    
    This commit adds that check before attempting creation of a file

--- admin/partials/nginx-helper-general-options.php
+++ admin/partials/nginx-helper-general-options.php
@@ -647,7 +647,7 @@ if ( is_network_admin() ) {
 			if ( ! is_dir( $log_path ) ) {
 				mkdir( $log_path );
 			}
-			if ( ! file_exists( $log_path . 'nginx.log' ) ) {
+			if ( is_writable( $log_path ) && ! file_exists( $log_path . 'nginx.log' ) ) {
 				$log = fopen( $log_path . 'nginx.log', 'w' );
 				fclose( $log );
 			}
