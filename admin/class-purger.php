7b4f088cdb337065e4888d99a7107ea985e4c4a0
Arafat Islam <80309866+arafatkn@users.noreply.github.com>
2022-03-25 19:03:35 +0600

    Fixed indentation

--- admin/class-purger.php
+++ admin/class-purger.php
@@ -263,7 +263,7 @@ abstract class Purger {
 
 			}
 
-            $post_types = get_post_types( array('public' => true) );
+            		$post_types = get_post_types( array('public' => true) );
 
 			if ( in_array($_post_type, $post_types, true) ) {
 
