d0e45eb234eb0ab2e0f6a82b976fbcbd1e62f9a2
sid177 <siddharthantikekar@gmail.com>
2023-04-28 18:07:19 +0530

    Fix php 8.1 compatibility and phpcs issues

--- admin/class-nginx-helper-admin.php
+++ admin/class-nginx-helper-admin.php
@@ -679,12 +679,20 @@ class Nginx_Helper_Admin {
 
 		global $nginx_purger, $wp;
 
-		$method = filter_input( INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_STRING );
+		$method = null;
+		if ( isset( $_SERVER['REQUEST_METHOD'] ) ) {
+			$method = wp_strip_all_tags( $_SERVER['REQUEST_METHOD'] );
+		}
 
+		$action = '';
 		if ( 'POST' === $method ) {
-			$action = filter_input( INPUT_POST, 'nginx_helper_action', FILTER_SANITIZE_STRING );
+			if ( isset( $_POST['nginx_helper_action'] ) ) {
+				$action = wp_strip_all_tags( $_POST['nginx_helper_action'] );
+			}
 		} else {
-			$action = filter_input( INPUT_GET, 'nginx_helper_action', FILTER_SANITIZE_STRING );
+			if ( isset( $_GET['nginx_helper_action'] ) ) {
+				$action = wp_strip_all_tags( $_GET['nginx_helper_action'] );
+			}
 		}
 
 		if ( empty( $action ) ) {
@@ -725,12 +733,12 @@ class Nginx_Helper_Admin {
 
 		if ( 'purge' === $action ) {
 	
-		    /**
-		     * Fire an action after the entire cache has been purged whatever caching type is used.
-		     * 
-		     * @since 2.2.2
-		     */
-		    do_action( 'rt_nginx_helper_after_purge_all' );
+			/**
+			 * Fire an action after the entire cache has been purged whatever caching type is used.
+			 * 
+			 * @since 2.2.2
+			 */
+			do_action( 'rt_nginx_helper_after_purge_all' );
 
 		}
 
