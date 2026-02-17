f84a7c2579a669e21cc1dc89886e0b97c44c19d7
Indira Biswas <indirabiswas@Indiras-MacBook-Air.local>
2026-02-17 10:27:51 +0530

    chore: remove unmaintained e2e-playwright tests and workflow

--- tests/e2e-playwright/e2e-test-utils-playwright/build-types/request-utils/posts.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Delete all posts using REST API.
- *
- * @this {import('./index').RequestUtils}
- */
-export function deleteAllPosts(): Promise<void>;
-//# sourceMappingURL=posts.d.ts.map
--- tests/e2e-playwright/e2e-test-utils-playwright/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { Admin } from './admin';
-export { Editor } from './editor';
-export { PageUtils } from './page-utils';
-export { RequestUtils } from './request-utils';
-export { test, expect } from './test';
